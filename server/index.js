1b5ab0695c27aa22a5501fc2a372a4dccae96c74
Alex Moreton <lxmrtn7@gmail.com>
2020-05-15 19:32:25 +0100

    Add Heroku post build for client

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
@@ -10,11 +11,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("This is the boilerplate-101 api"));
 
+// This handles react router in production when it get bundled
+// to build folder, we need to declare them here for serving them in prod
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// This is for simple express form
 app.post("/contact-form", (req, res) => {
   const stringifyResponse = `${JSON.stringify(req.body.post)} \n`;
 
   try {
-    fs.appendFileSync("client/Public/userList.txt", stringifyResponse, {
+    fs.appendFileSync("client/public/userList.txt", stringifyResponse, {
       flag: "a",
     });
   } catch (e) {
@@ -26,6 +32,11 @@ app.post("/contact-form", (req, res) => {
       req.body.post
     )}`
   );
+});
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
