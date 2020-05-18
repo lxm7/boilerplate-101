8cafa6da2a992ec11b4d18ef074aa43db99ea1b8
Alex Moreton <lxmrtn7@gmail.com>
2020-05-18 19:57:24 +0100

    Updates

---
+++ precache-manifest.239d03ead70443290d97cbf37d1d539a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fad6c82679bd1ba67182df21c537f2",
+    "url": "/boilerplate-101/index.html"
+  },
+  {
+    "revision": "ec2d401883b2f61b108e",
+    "url": "/boilerplate-101/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "61557825448c56acb1a1",
+    "url": "/boilerplate-101/static/css/main.3cbd6e62.chunk.css"
+  },
+  {
+    "revision": "ec2d401883b2f61b108e",
+    "url": "/boilerplate-101/static/js/2.d92d4510.chunk.js"
+  },
+  {
+    "revision": "af87b9e68f630eae14680b6f08a2989d",
+    "url": "/boilerplate-101/static/js/2.d92d4510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61557825448c56acb1a1",
+    "url": "/boilerplate-101/static/js/main.2982f61c.chunk.js"
+  },
+  {
+    "revision": "70a48c1d1d380021338a",
+    "url": "/boilerplate-101/static/js/runtime-main.3723d3b6.js"
+  },
+  {
+    "revision": "4430fc7517f7d4aee4550e26db44ddbe",
+    "url": "/boilerplate-101/static/media/destinations.4430fc75.png"
+  }
+]);
